073b971c45c186567de1d3b974c6e0b38f0cebe3
ChristianMacDonald <chrismacd412@gmail.com>
2019-07-30 22:01:34 -0400

    Added functionality to get requests on /api/users endpoint

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require('express');
 
+const userRouter = require('./users/userRouter');
+
 const server = express();
+
+function logger(req, res, next) {
+    console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url}`);
+    next();
+}
+
+server.use(logger);
+
+server.use('/api/users', userRouter);
+
+server.get('/', (req, res) => {
+    res.send('webapi-iii-challenge');
+});
 
 server.listen(8000, () => {
     console.log("API running on port 8000!");
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,4 +1,5 @@
-const express = 'express';
+const express = require('express');
+const userDb = require('./userDb');
 
 const router = express.Router();
 
@@ -11,15 +12,23 @@ router.post('/:id/posts', (req, res) => {
 });
 
 router.get('/', (req, res) => {
-
+    userDb.get().then(users => {
+        res.status(200).json(users);
+    }).catch(error => {
+        res.status(500).json({ error: 'The users information could not be retrieved.' });
+    });
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', validateUserId, (req, res) => {
+    res.status(200).json(req.user);
 });
 
-router.get('/:id/posts', (req, res) => {
-
+router.get('/:id/posts', validateUserId, (req, res) => {
+    userDb.getUserPosts(req.params.id).then(posts => {
+        res.status(200).json(posts);
+    }).catch(error => {
+        res.status(500).json({ error: 'The posts information could not be retrieved.' });
+    });
 });
 
 router.delete('/:id', (req, res) => {
@@ -33,7 +42,14 @@ router.put('/:id', (req, res) => {
 //custom middleware
 
 function validateUserId(req, res, next) {
-
+    userDb.getById(req.params.id).then(user => {
+        if (user) {
+            req.user = user;
+            next();
+        } else {
+            res.status(404).json({ message: 'The user with the specified ID does not exist.' });
+        }
+    });
 };
 
 function validateUser(req, res, next) {
